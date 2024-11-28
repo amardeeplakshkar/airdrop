aa3409d1458b55d43268de6f342345611aeba08a
Amardeep Lakshkar <info.amardeeppvt@gmail.com>
2024-11-28 20:44:26 +0530

    Added: Website Logo

--- src/content/config.ts
+++ src/content/config.ts
@@ -8,7 +8,7 @@ const airdrops = defineCollection({
     pubDate: z.date(),
     endDate: z.date().optional(),
     link: z.string().optional(),
-    value: z.string(),
+    value: z.string().optional(),
     requirements: z.array(z.string()),
     image: z.string(),
     status: z.enum(['active', 'ended', 'upcoming']),
