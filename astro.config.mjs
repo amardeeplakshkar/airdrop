c82dfc378cf9d264a6c5ecef03efa76a3d1321e8
Amardeep Lakshkar <info.amardeeppvt@gmail.com>
2024-11-28 15:16:07 +0530

    Sitemap Added

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,19 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import mdx from '@astrojs/mdx';
+import node from '@astrojs/node'; // Import the Node adapter
 
 export default defineConfig({
   integrations: [
     tailwind(),
     mdx()
   ],
+  output: 'server',  // Ensure server-side rendering for dynamic pages
+  adapter: node({
+  mode: 'standalone',}),
   markdown: {
     shikiConfig: {
       theme: 'dracula'
     }
-  }
+  },
 });
