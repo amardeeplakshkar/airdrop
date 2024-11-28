c8be299c139e0544aab9eaa1386ad41ec91d38d4
Amardeep Lakshkar <info.amardeeppvt@gmail.com>
2024-11-28 19:00:29 +0530

    Sitemap Added

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,11 +5,31 @@ import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
   site: 'https://airdropwatch.vercel.app',
-  integrations: [tailwind(), mdx(), sitemap({
+  integrations: [tailwind(), mdx(), 
+    sitemap({
       changefreq: 'weekly',
       priority: 0.8,
       lastmod: new Date('2024-11-28'),
-  })],
+      filter(item) {
+        // Exclude any unwanted routes here
+        return true;
+      },
+      serialize(item) {
+        if (item.url === 'https://airdropwatch.vercel.app/guide/') {
+          item.priority = 0.9;
+        }
+        // Home page priority
+        if (item.url === 'https://airdropwatch.vercel.app/') {
+          item.priority = 1.0;
+        }
+
+        // /airdrops priority
+        if (item.url === 'https://airdropwatch.vercel.app/airdrops/') {
+          item.priority = 0.9;
+        }
+        return item; 
+      },
+    }),],
   markdown: {
     shikiConfig: {
       theme: 'dracula'
